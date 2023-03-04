6dcf927f92c6d352e564fd29eeb933874dc41e53
Daemon <clement.lecoeur34@gmail.com>
2023-03-04 03:07:52 +0100

    Fixing texture flickering

--- Shaders/skybox.fs
+++ Shaders/skybox.fs
@@ -15,25 +15,24 @@ uniform sampler2D uSampler5;
 
 void main(void)
 {
-
-  vec4 col = vec4(0,0,0,1);
-  if(vPosOrig.z==uTailleBoite){
-   col = texture2D(uSampler0, vec2(texCoords.s, texCoords.t));
- }
- else if(vPosOrig.z==-uTailleBoite){
-   col = texture2D(uSampler1, vec2(texCoords.s, texCoords.t));
- }
- else if(vPosOrig.y==uTailleBoite){
-   col = texture2D(uSampler2, vec2(texCoords.s, texCoords.t));
- }
- else if(vPosOrig.y==-uTailleBoite){
-   col = texture2D(uSampler3, vec2(texCoords.s, texCoords.t));
- }
- else if(vPosOrig.x==-uTailleBoite){
-  col = texture2D(uSampler4, vec2(texCoords.s, texCoords.t));
-}
-else{
-	col = texture2D(uSampler5, vec2(texCoords.s, texCoords.t));
-}
-gl_FragColor = col;
+	vec4 col = vec4(0,0,0,1);
+	if(vPosOrig.z > (uTailleBoite - 0.1) && vPosOrig.z < (uTailleBoite + 0.1)){
+		col = texture2D(uSampler0, vec2(texCoords.s, texCoords.t));
+	}
+	else if(vPosOrig.z > (-uTailleBoite - 0.1) && vPosOrig.z < (-uTailleBoite + 0.1)){
+		col = texture2D(uSampler1, vec2(texCoords.s, texCoords.t));
+	}
+	else if(vPosOrig.y > (-uTailleBoite - 0.1) && vPosOrig.y < (-uTailleBoite + 0.1)){
+		col = texture2D(uSampler2, vec2(texCoords.s, texCoords.t));
+	}
+	else if(vPosOrig.y > (uTailleBoite - 0.1) && vPosOrig.y < (uTailleBoite + 0.1)){
+		col = texture2D(uSampler3, vec2(texCoords.s, texCoords.t));
+	}
+	else if(vPosOrig.x > (uTailleBoite - 0.1) && vPosOrig.x < (uTailleBoite + 0.1)){
+		col = texture2D(uSampler4, vec2(texCoords.s, texCoords.t));
+	}
+	else {
+		col = texture2D(uSampler5, vec2(texCoords.s, texCoords.t));
+	}
+	gl_FragColor = col;
 }
